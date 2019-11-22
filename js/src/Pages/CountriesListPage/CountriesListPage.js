da18f37f45b5008c0526ddc8ba51ee5956aa84f0
PawelSzy <peal1982@gmail.com>
2019-11-22 10:13:55 +0100

    Add search for countries

--- js/src/Pages/CountriesListPage/CountriesListPage.js
+++ js/src/Pages/CountriesListPage/CountriesListPage.js
@@ -1,32 +1,53 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import Countries from '../../Components/Countries/Countries'
+import {Form} from 'react-bootstrap';
 
 class CountriesListPage extends Component {
   state = {
-    countries: []
+    countries: [],
+    searchPhrase: 'Poland'
   };
 
   componentDidMount() {
-    //const url = `http://localhost:8080/countries/Korea`;
-    const url = `http://localhost:8080/countries/Andorra`;
-    axios.get(url, { crossdomain: true })
+    this.getCountries();
+  }
+
+  getCountries = () => {
+    const url = `http://localhost:8080/countries/${this.state.searchPhrase}`;
+    axios.get(url, {crossdomain: true})
       .then(response => response.data)
-      .then(data => {this.setState({ countries: data })
-        console.log(data)}
+      .then(data => {
+        this.setState({countries: data})
+        }
       )
       .catch(error => console.error(error));
+  }
+
+  handleChange = (event) => {
+    this.setState({searchPhrase: event.target.value})
+  }
+
+  handleKeyUp = (event) => {
+    if (event.keyCode == 13) {
+      this.getCountries();
+    }
   }
 
   render() {
     return (
       <div>
         <h1>Countries</h1>
-
+        <Form.Control
+          type="text"
+          placeholder="Find country by name, code or language"
+          className="col-md-4 my-4"
+          onKeyUp={(e) => this.handleKeyUp(e)}
+          onChange={(e) => this.handleChange(e)} />
         <Countries countries={this.state.countries}/>
     </div>
-  )
-}
+   )
+  }
 }
 
 export default CountriesListPage
